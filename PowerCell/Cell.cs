a1639e169b398c9907e73b048bc2ac9095b5260b
Gonçalo <goncaloalmeida.artwork@gmail.com>
2026-04-23 16:30:24 +0000

    Acho que está feito

--- PowerCell/Cell.cs
+++ PowerCell/Cell.cs
@@ -9,22 +9,54 @@ namespace PowerCell
     {
         private float charge;
         public readonly string Name;
-        public float Charge;
-        public readonly int Level;
+        public float Charge
+        {
+            get
+            {
+                if(charge < 0)
+                {
+                    return 0;
+                } 
+                else if(charge > 200)
+                {
+                    return 200;
+                }
+                else
+                {
+                    return charge;
+                }
+            }
+        }
+        public int Level
+        {
+            get
+            {
+                return  1 + (int)charge/40;
+            }
+        }
+
+
 
         public Cell(string nome)
         {
             Name = nome;
+            charge = 200;
         }
 
             public void Consume(float amount)
         {
-            amount = amount - Charge;
+
+            charge = charge - amount;
         }
 
         public void Restore()
         {
-            Charge = 200;
+            charge = 200;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Name}] Level {Level}: {Charge:F0}/200";
         }
 
     }
---
+++ PowerCell/README.md
@@ -0,0 +1,15 @@
+```mermaid
+classDiagram 
+    class Cell {
+        -charge: float
+        +Name: string
+        +Charge: float
+        +Level: int
+
+
+        +Cell() void
+        +Restore() void
+        +ToString() string
+        
+    }
+```
